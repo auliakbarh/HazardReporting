8b0b87e6ffef517b847757140c5ac9fce06d7eb0
auliakbarh <auliaakbarharahaap@gmail.com>
2020-03-16 14:44:10 +0700

    add some basic screens

---
+++ src/screens/ListHazard.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, StatusBar} from 'react-native';
+import config from '../config';
+import {textStyles} from '../config/styles';
+
+export default class ListHazard extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <StatusBar
+          barStyle="dark-content"
+          backgroundColor={config.color.background}
+        />
+        <View style={styles.mainContainer}>
+          <Text style={styles.text}>List Hazard</Text>
+        </View>
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+    backgroundColor: config.color.background,
+  },
+  text: textStyles.main,
+});
