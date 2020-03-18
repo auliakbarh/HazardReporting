acc7ba984af2c4996758f50582c6299daea037de
auliakbarh <auliaakbarharahaap@gmail.com>
2020-03-18 19:08:24 +0700

    pass props to children (SimpleApp)

--- src/router/routerConfig.js
+++ src/router/routerConfig.js
@@ -1,14 +1,20 @@
 import {NavigationContainer} from '@react-navigation/native';
 import * as React from 'react';
 
+import {YellowBox} from 'react-native';
+
+YellowBox.ignoreWarnings([
+  'Non-serializable values were found in the navigation state',
+]);
+
 import SimpleApp from './bottomTabNavigator';
 
-function RouterApp(props) {
-  return (
+const RouterApp = props => {
+  return () => (
     <NavigationContainer>
-      <SimpleApp />
+      <SimpleApp {...props} />
     </NavigationContainer>
   );
-}
+};
 
 export default RouterApp;
