39f1b9e983b841c0898563eaf42ae5fac090ef70
auliakbarh <auliaakbarharahaap@gmail.com>
2020-03-18 19:07:52 +0700

    add database as props to router

--- index.js
+++ index.js
@@ -4,7 +4,11 @@
 
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
-import App from './src/router/routerConfig';
+import routerConfig from './src/router/routerConfig';
 import {name as appName} from './app.json';
+
+import database from './src/models/database';
+
+const App = routerConfig({database});
 
 AppRegistry.registerComponent(appName, () => App);
